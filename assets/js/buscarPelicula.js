8ea223b7f86c738d8a3e7097b26cb770b804862f
nicomansotti <mansottinicolas@gmail.com>
2020-10-07 17:26:03 -0300

    agrego funcionalidad de mandar mail a un amigo. falta definir como van a ser las validaciones

--- assets/js/buscarPelicula.js
+++ assets/js/buscarPelicula.js
@@ -1,9 +1,11 @@
-tres = 0; 
+//tres = 0; 
 
 
 function buscarPeli(){
 
-    tres = tres + 2;
+
+    //tres = tres + 10;
+
 
     peliculaBuscador = document.getElementById('peliculaBuscador').value ;
 
@@ -18,23 +20,26 @@ function buscarPeli(){
 
                 var buscarMas = $("<div><button onclick='buscarPeli()' id='buscarMas' >Buscar más resultados</button>");
                 var resultHtml = $("<div><h2>Resultado de busqueda:</h2>");
-                //for (i = 0; i < data["results"].length; i++) {
-                 
-                for (i = 0; i < tres; i++) {
 
+                for (i = 0; i < data["results"].length; i++) {
+                
+                //for (i = 0; i < tres; i++) {
+
+                    
    
                     titulo = data["results"][i]["title"];
                     imagen = data["results"][i]["poster_path"];
                     descripcion = data["results"][i]["overview"];
                     id = data["results"][i]["id"];            
-                    resultHtml.append("<div><h3>" + titulo + "</h3><img src='https://image.tmdb.org/t/p/w500" + imagen + "'></div>" + "<h4>" + descripcion + "</h3>" +  "<a id='sharebutton' onclick='movieSelected("+id+")' href='#'>Movie Details</a>")
+                    resultHtml.append("<div><h3>" + titulo + "</h3><img src='https://image.tmdb.org/t/p/w500" + imagen + "'></div>" + "<h4>" + descripcion + "</h3>" +  "<a id='sharebutton' onclick='movieSelected("+id+")' href='#'>Movie Details</a><div><a onclick='mandarAmigo("+id+")' href='#'> Compartir con un amigo </a></div>")
                     // PUEDE QUEDAR PARA LA RESEÑA"<a id=sharebutton href=share.html> Compartir con un amigo </a>"                                                                        
                     //resultHtml.append("<div><img src='https://image.tmdb.org/t/p/w500" + imagen + "'></div>")
                 }
+
                 
   
                 resultHtml.append("</div>");
-                buscarMas.append("</div>")
+                buscarMas.append("</div>");
                 $("#movies").html(resultHtml);
                 $("#buscarMas").html(buscarMas);
 
@@ -57,7 +62,7 @@ $(function () {
     $('#emailLink').on('click', function (event) {
         event.preventDefault();
       var email = $('#emailb').val();
-      var subject = 'Circle Around';
+      var subject = $('#subject').val();
       var emailBody = $('#comentario').val(); 
       window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
     });
@@ -65,6 +70,12 @@ $(function () {
 
 
 
+
+function mandarAmigo(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'share.html';
+    return false;
+  }
 
 
 //copiado de la pagina y adaptado
@@ -85,20 +96,66 @@ $(function () {
             success: function(data) {
 
 
-
-                popularity = data["popularity"];
+                
+                titulo = data["title"];
                 vote_average = data["vote_average"];
-                descripcion = data["overview"];
+                lanzamiento = data["release_date"];
                 
 
 
      
-      let output = '<div>Popularidad: '+popularity+'</div><div>Puntaje: '+vote_average+'</div>'+'</div><div>Sinapsis: '+descripcion+'</div>';
-
-
+      let output = '<div>Titulo: '+titulo+'</div><div>Puntaje: '+vote_average+'</div>'+'</div><div>Lanzamiento: '+lanzamiento+'</div>';
+      
+    
 
 
       $('#movie').html(output);
+
+
+       },
+            error: function(err) {
+                alert(JSON.stringify(err));
+            }
+        });
+  }
+
+
+
+
+
+  function llenarCamposMail(){
+      
+    let movieId = sessionStorage.getItem('movieId');
+   
+        $.ajax({
+            url: "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=es-MX" ,
+            contentType: "application/json",
+            type: "GET",
+            success: function(data) {
+
+
+
+                popularity = data["popularity"];
+                vote_average = data["vote_average"];
+                descripcion = data["overview"];
+
+               titulo = data["title"];
+               //vote_average = data["vote_average"];
+                lanzamiento = data["release_date"];
+                
+
+
+
+
+     
+      //let output = '<div>Popularidad: '+popularity+'</div><div>Puntaje: '+vote_average+'</div>'+'</div><div>Sinapsis: '+descripcion+'</div>';
+
+
+      let output = '<label>Mail suyo</label><input id="email" type="text" placeholder="Introduzca su Email"><label>Mail de su amigo</label><input id="emailb" type="text" placeholder="Introduzca el Email de su amigo"><label>Asunto</label><input id="subject" type="text" placeholder="Introduzca un asunto" value="Titulo: '+titulo+' | Fecha Lanzamiento: '+lanzamiento+' | Puntuacion: '+vote_average+'"><label>Comentario</label><textarea rows="12" cols="50" name="comentario" id="comentario" placeholder="Deja un comentario...">Popularidad: '+popularity+'\n \nPuntaje: '+vote_average+'\n \nSinapsis: '+descripcion+'\n \nMira esta peli!!!</textarea>'
+
+      //document.getElementById('subject').value = "Popularidad: "+popularity+" - Puntaje: "+vote_average+" - Sinapsis: "+descripcion;
+
+      $('#compartirAmigoo').html(output);
 
 
        },
